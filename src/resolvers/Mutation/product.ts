fb4f19b1a713a9ac1b7711321f141e4cbec8f422
Hiu-Fung Hau <dafungz@htomail.com>
2018-06-04 09:29:18 +0200

    Implement updateProduct & deleteProduct

--- src/resolvers/Mutation/product.ts
+++ src/resolvers/Mutation/product.ts
@@ -1,4 +1,5 @@
 import shortid from 'shortid'
+import { forwardTo } from 'prisma-binding'
 import { createWriteStream } from 'fs'
 import { getUserId, Context } from '../../utils'
 
@@ -20,10 +21,17 @@ const storeUpload = async ({ stream, filename }): Promise<any> => {
   )
 }
 
+interface ProductData {
+  name?: string;
+  price?: number;
+  pictureUrl?: string;
+}
+
 export const product = {
   async createProduct(parent, { name, price, picture }, ctx: Context, info) {
     const userId = getUserId(ctx)
     console.log('create product')
+    
     return ctx.db.mutation.createProduct(
       {
         data: {
@@ -37,5 +45,38 @@ export const product = {
       },
       info
     )
-  }
+  },
+
+  async updateProduct(parent, { id, name, price, picture }, ctx: Context, info) {
+    const userId = getUserId(ctx)
+    const product = await ctx.db.query.product({ where: { id } }, `{ seller { id } }`)
+    console.log(product)
+    if (userId !== product.seller.id) {
+      throw new Error('Not authorized')
+    }
+
+    let pictureUrl = null
+    if (picture) {
+      pictureUrl = await processUpload(picture)
+    }
+
+    const data: ProductData = {};
+
+    if (name) data.name = name
+    if (price) data.price = price
+    if (pictureUrl) data.pictureUrl = pictureUrl
+
+    console.log('update product')
+    return ctx.db.mutation.updateProduct(
+      {
+        data,
+        where: {
+          id
+        }
+      },
+      info
+    )
+  },
+
+  deleteProduct: forwardTo('db')
 }
